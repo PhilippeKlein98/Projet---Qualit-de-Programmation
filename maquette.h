abbc653650bd1a1bdd5b956573951a73c53af5e5
danielpfd <dany.pfd@gmail.com>
2017-12-12 11:01:05 +0100

    fichier maquette temporaire

---
+++ maquette.h
@@ -0,0 +1,25 @@
+#ifndef MAQUETTE_H_INCLUDED
+#define MAQUETTE_H_INCLUDED
+#include <vector>
+#include<iostream>
+
+class maquette
+{
+public:
+	maquette();
+	int totalCreditECTS() const;
+    int volumeHoraireTotal()const;
+	int nombreHeureDeCours()const;
+	int nombreHeureDeTD()const;
+	int nombreHeureDeTP()const;
+	UE* operator[](int codeUE) const;
+    UE* operator[](int codeUE);
+	void ajouterUE(UE*);
+	void supprimerUE(int codeUE);
+	void print(std::ostream& ost) const ;
+
+private:
+    std::vector<UE*> d_listeUE;
+};
+
+#endif // MAQUETTE_H_INCLUDED
