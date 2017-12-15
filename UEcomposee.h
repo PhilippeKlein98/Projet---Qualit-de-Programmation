261df1df4a3bb65678ab986a54ce719419a1fe7a
PhilippeKlein98 <33924500+PhilippeKlein98@users.noreply.github.com>
2017-12-15 08:26:45 +0100

    Update UEcomposee.h

--- UEcomposee.h
+++ UEcomposee.h
@@ -10,9 +10,18 @@ class UEcomposee : public UE
 {
     public:
         UEcomposee();
+        virtual std::string intitule() const override ;
+        virtual std::string code() const override ;
+        virtual int coefficient() const override ;
+        virtual int nombreHeuresCM() const override ;
+        virtual int nombreHeuresTD() const override ;
+        virtual int nombreHeuresTP() const override ;
+        virtual void print(std::ostream& ost) const override ;
 
     private:
-        std::vector<matiere*> d_tabMatieres;
+        std::string d_intitule ;
+        std::string d_code ;
+        std::vector<matiere*> d_listeMatieres;
 };
 
 #endif // UECOMPOSEE_H
