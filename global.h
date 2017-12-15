66922fcc652ef6ee29c5e85a697e348ca2ae775a
PhilippeKlein98 <33924500+PhilippeKlein98@users.noreply.github.com>
2017-12-15 08:41:42 +0100

    Add files via upload

---
+++ global.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL_H_INCLUDED
+#define GLOBAL_H_INCLUDED
+
+#include <iostream>
+
+namespace gestionUE
+{
+    const double CM_VERS_TD = 2.0/3.0 ;
+    const double TP_VERS_TD = 3.0/2.0 ;
+}
+
+
+#endif // GLOBAL_H_INCLUDED
