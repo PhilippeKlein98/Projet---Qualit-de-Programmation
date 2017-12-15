35cd8373d2e840bf2a6289fa456d077f9d525edf
PhilippeKlein98 <33924500+PhilippeKlein98@users.noreply.github.com>
2017-12-15 09:15:51 +0100

    Update UEchoix.h

--- UEchoix.h
+++ UEchoix.h
@@ -1,17 +1,19 @@
 #ifndef UECHOIX_H
 #define UECHOIX_H
 
+#include "global.h"
 
 class UEchoix : public UE
 {
     public:
         UEchoix();
         virtual ~UEchoix();
-        virtual std::string intitule() const = 0 ;
+        virtual std::string intitule() const override ;
         virtual int nombreHeuresCM() const = 0 ;
         virtual int nombreHeuresTD() const = 0 ;
-virtual int nombreHeuresTP() const = 0 ;
+        virtual int nombreHeuresTP() const = 0 ;
     private:
+        std::string intitule ;
         std::vector<UE*> d_listeUE ;
 };
 
