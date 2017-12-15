453620912550447d43fabbab5ddb40175fd9fc22
PhilippeKlein98 <33924500+PhilippeKlein98@users.noreply.github.com>
2017-12-15 09:40:05 +0100

    Update UE.h

--- UE.h
+++ UE.h
@@ -11,7 +11,7 @@ namespace gestionUE
             UE(int ETCS , int coefficient);
             virtual ~UE() = default;
             virtual std::string code() const = 0 ;
-            int coefficient() const ;
+            virtual int coefficient() const = 0 ;
             int ECTS() const ;
             virtual std::string intitule() const = 0 ;
             virtual int nombreHeuresCM() const = 0 ;
@@ -23,7 +23,6 @@ namespace gestionUE
 
         private:
             int d_ETCS;
-            int d_coefficient ;
 
     };
 }
