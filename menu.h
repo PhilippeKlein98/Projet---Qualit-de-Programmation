63438edbc328b331eadd3f8b9b6fb2da3ec777e1
danielpfd <dany.pfd@gmail.com>
2017-12-18 11:46:56 +0100

    Add files via upload

---
+++ menu.h
@@ -0,0 +1,24 @@
+#ifndef MENU_H
+#define MENU_H
+#include <string>
+
+namespace gestionUE{
+	
+class menu
+{
+    public:
+        void menuPrincipal();
+		void menuFormations();
+		void menuMaquettes();
+		void menuUEs();
+		
+		int testInt(const std::string& s);
+
+    private:
+        
+};
+
+}
+
+
+#endif // MENU_H
