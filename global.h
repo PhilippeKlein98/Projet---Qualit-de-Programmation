32929d93d876a3346dd3758b18f76042c91a11a0
PhilippeKlein98 <33924500+PhilippeKlein98@users.noreply.github.com>
2017-12-18 11:27:24 +0100

    Ajout des constantes pour la sauvegarde dans les fichiers

--- global.h
+++ global.h
@@ -7,6 +7,10 @@ namespace gestionUE
 {
     const double CM_VERS_TD = 2.0/3.0 ;
     const double TP_VERS_TD = 3.0/2.0 ;
+
+    const unsigned int UE_SIMPLE = 1 ;
+    const unsigned int UE_COMPOSEE = 2 ;
+    const unsigned int UE_CHOIX = 3 ;
 }
 
 
