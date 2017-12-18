63063ada2293a5ad1c1c01c10ce100c9f72e1d6f
danielpfd <dany.pfd@gmail.com>
2017-12-18 09:51:20 +0100

    correction + ajout operateur

--- formation.h
+++ formation.h
@@ -4,16 +4,19 @@
 #include <vector>
 #include "maquette.h"
 
+namespace gestionUE{
+	
 class formation
 {
     public:
         formation();
-        std::string codeFormation() const;
+        int	 codeFormation() const;
         std::string intituleFormation() const;
         int totalCreditECTS() const;
         maquette* operator[](int i) const;
         maquette* operator[](int i);
-        void print(std::ostream& ost) const ;
+        void print(std::ostream& ost) const;
+	
 
     private:
         std::vector<maquette*> d_maquettes;
@@ -21,5 +24,9 @@ class formation
 		std::string d_intituleFormation;
 
 };
+
+}
+
+std::ostream& operator<<(std::ostream& ost, formation& f);
 
 #endif // FORMATION_H
