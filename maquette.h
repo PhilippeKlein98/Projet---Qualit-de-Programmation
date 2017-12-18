e7983486e8e86ef73944cc696f8fe411c6ca3756
PhilippeKlein98 <33924500+PhilippeKlein98@users.noreply.github.com>
2017-12-18 10:08:50 +0100

    Add files via upload

--- maquette.h
+++ maquette.h
@@ -4,24 +4,29 @@
 #include <iostream>
 #include "UE.h"
 
-class maquette
+namespace gestionUE
 {
-public:
-	maquette();
-	int totalCreditECTS() const;
-	double nombreHeuresTotalEnTD() const ;
-    	int nombreHeuresTotal()const;
-	int nombreHeuresCM()const;
-	int nombreHeuresTD()const;
-	int nombreHeuresTP()const;
-	UE* operator[](int codeUE) const;
-    	UE* operator[](int codeUE);
-	void ajouterUE(UE*);
-	void supprimerUE(int codeUE);
-	void print(std::ostream& ost) const ;
+    class maquette
+    {
+    public:
+        maquette();
+        int totalCreditECTS() const;
+        double nombreHeuresTotalEnTD() const ;
+            int nombreHeuresTotal()const;
+        int nombreHeuresCM()const;
+        int nombreHeuresTD()const;
+        int nombreHeuresTP()const;
+        UE* operator[](int codeUE) const;
+        UE* operator[](int codeUE);
+        void ajouterUE(UE*);
+        void supprimerUE(int codeUE);
+        void print(std::ostream& ost) const ;
 
-private:
-    std::vector<UE*> d_listeUE;
-};
+    private:
+        std::vector<UE*> d_listeUE;
+    };
+}
+
+
 
 #endif // MAQUETTE_H_INCLUDED
