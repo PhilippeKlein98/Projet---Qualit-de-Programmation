4f61ac20b689cd316b4baf63d41308fde36d6779
AveRomain <33924524+AveRomain@users.noreply.github.com>
2018-01-09 10:48:04 +0100

    Rectification

--- testMatiere.cpp
+++ testMatiere.cpp
@@ -2,7 +2,7 @@
 #include "matiere.h"
 #include "global.h"
 
-TEST_CASE("La matiere est bien construite", "[matiere]")
+TEST_CASE("Tests sur la classe matiere", "[matiere]")
 {
     std::string code = "123456789";
     std::string nom = "Matiere";
@@ -12,46 +12,33 @@ TEST_CASE("La matiere est bien construite", "[matiere]")
     int heuresTP = 12;
     int taille = gestionUE::matiere::listeMatieres.size();
 
-    gestionUE::matiere::creerMatiere( code , nom , coefficient , heuresCM , heuresTD , heuresTP );
+    SECTION("La matiere est bien construite", "[matiere]")
+    {
+        gestionUE::matiere::creerMatiere( code , nom , coefficient , heuresCM , heuresTD , heuresTP );
 
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->code() == code);
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->nom() == nom);
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->coefficient() == coefficient);
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresCM() == heuresCM);
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTD() == heuresTD);
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTP() == heuresTP);
-}
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->code() == code);
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->nom() == nom);
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->coefficient() == coefficient);
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresCM() == heuresCM);
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTD() == heuresTD);
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTP() == heuresTP);
+    }
 
-TEST_CASE("Le nombre total d'heure bien calculer", "[matiere]")
-{
-    std::string code = "123456789";
-    std::string nom = "Matiere";
-    int coefficient = 5;
-    int heuresCM = 10;
-    int heuresTD = 11;
-    int heuresTP = 12;
-    int taille = gestionUE::matiere::listeMatieres.size();
+    SECTION("Le nombre total d'heure bien calcule", "[matiere]")
+    {
+        int heuresTotal = heuresTD + heuresCM + heuresTP;
 
-    int heuresTotal = heuresTD + heuresCM + heuresTP;
+        gestionUE::matiere::creerMatiere( code , nom , coefficient , heuresCM , heuresTD , heuresTP );
 
-    gestionUE::matiere::creerMatiere( code , nom , coefficient , heuresCM , heuresTD , heuresTP );
+        REQUIRE(gestionUE::matiere::listeMatieres[taille-1]->nombreHeuresTotal() == heuresTotal);
+    }
 
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTotal() == heuresTotal);
-}
+    SECTION("Le nombre total d'heure TD bien calcule", "[matiere]")
+    {
+        double heuresTotalEnTD = heuresCM * gestionUE::CM_VERS_TD + heuresTD + heuresTP * gestionUE::TP_VERS_TD;
 
-TEST_CASE("Le nombre total d'heure TD bien bien calculer", "[matiere]")
-{
-    std::string code = "123456789";
-    std::string nom = "Matiere";
-    int coefficient = 5;
-    int heuresCM = 10;
-    int heuresTD = 11;
-    int heuresTP = 12;
-    int taille = gestionUE::matiere::listeMatieres.size();
+        gestionUE::matiere::creerMatiere( code , nom , coefficient , heuresCM , heuresTD , heuresTP );
 
-    int heuresTotalEnTD = heuresCM * gestionUE::CM_VERS_TD + heuresTD + heuresTP* gestionUE::TP_VERS_TD;
-
-    gestionUE::matiere::creerMatiere( code , nom , coefficient , heuresCM , heuresTD , heuresTP );
-
-    REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTotalEnTD() == heuresTotalEnTD);
+        REQUIRE(gestionUE::matiere::listeMatieres[taille]->nombreHeuresTotalEnTD() == heuresTotalEnTD);
+    }
 }
